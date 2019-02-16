86b21be417822d00b84370fb2b5056239a1cc3d3
sanduni-E <sanduniue@gmail.com>
2019-02-16 23:09:56 +0530

    For-loop-begin
    For-loop-begin

---
+++ src/pkgfor/loop/begin/ForLoopBegin.java
@@ -0,0 +1,28 @@
+
+package pkgfor.loop.begin;
+
+
+public class ForLoopBegin {
+
+    public static void main(String[] args) {
+        {
+            System.out.println("If number want to increase");
+        }
+        for(int x=0;x<10;x++){
+            System.out.println(x);
+        }
+        {
+            System.out.println("Next,if number wants to discrease");
+        }
+        for(int y=6;y>2;y--){
+            System.out.println(y);
+        }
+        {
+            System.out.println("When count must stop at given number");
+        }
+        for(int z=0;z<=5;z++){
+            System.out.println(z);
+        }
+    }
+    
+}
